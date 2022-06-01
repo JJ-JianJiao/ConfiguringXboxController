643565a332031c58c55a707789b38dc35628f6ad
Jiao Jian <jj.jianjiao@gmail.com>
2022-06-01 12:17:47 -0600

    Initial commit

---
+++ Assets/Cude.cs
@@ -0,0 +1,57 @@
+//https://www.youtube.com/watch?v=p-3S73MaDP8
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+
+public class Cude : MonoBehaviour
+{
+    InputMaster controls;
+
+    Vector2 move;
+    Vector2 rotate;
+
+    private void Awake()
+    {
+        controls = new InputMaster();
+
+        controls.Player.Attack.performed += ctx => Grow();
+        controls.Player.Jump.performed += ctx => TurnSmall();
+
+        controls.Player.Movement.performed += ctx => move = ctx.ReadValue<Vector2>();
+
+        controls.Player.Movement.canceled += ctx => move = Vector2.zero;
+
+        controls.Player.Rotate.performed += ctx => rotate = ctx.ReadValue<Vector2>();
+        controls.Player.Rotate.canceled += ctx => rotate = Vector2.zero;
+    }
+
+    private void Update()
+    {
+        Vector2 m = new Vector2(-move.x, move.y) * Time.deltaTime;
+        transform.Translate(m, Space.World);
+
+        Vector2 v = new Vector2(-rotate.y, -rotate.x) * 100 * Time.deltaTime;
+        transform.Rotate(new Vector3(v.x,v.y,0));
+    }
+
+    void Grow() {
+        transform.localScale *= 1.1f;
+    }
+
+    void TurnSmall() {
+        transform.localScale /= 1.1f;
+    }
+
+    private void OnEnable()
+    {
+        controls.Player.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Player.Disable();
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# ConfiguringXboxController
+ 
